eaf0a3a62835c7ca6395ce3955f1b73bfc51fc21
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2024-01-10 00:46:13 -0800

    Bullet update
    Bullets now disappear after hitting an enemy

--- Assets/Scripts/Top Down Scripts/Weapon_TopDown.cs
+++ Assets/Scripts/Top Down Scripts/Weapon_TopDown.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Weapon_TopDown : MonoBehaviour
 {
     #region Variables
-
     public float damage = 1;
+
+    public enum WeaponType { Melee, Bullet}
+    public WeaponType weaponType;
     #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -15,6 +17,10 @@ public class Weapon_TopDown : MonoBehaviour
         if(enemy != null)
         {
             enemy.TakeDamage(damage);
+            if(weaponType == WeaponType.Bullet)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
