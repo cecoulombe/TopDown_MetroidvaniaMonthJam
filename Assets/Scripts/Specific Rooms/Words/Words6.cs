850f0ceac980010008575a0c58b59780aeb3a5f1
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2024-02-24 19:55:15 -0800

    Words6

---
+++ Assets/Scripts/Specific Rooms/Words/Words6.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Words6 : MonoBehaviour
+{
+    #region Variables
+    private string currentRoom;
+    private string previousRoom;
+
+    [Header("Loading zones for each door")]
+    [SerializeField]
+    private PlayerController_TopDown player;
+
+    [SerializeField]
+    private GameObject spawnPoint;
+    [SerializeField]
+    private GameObject Overwhelm4_LoadingZone;
+    [SerializeField]
+    private GameObject Words5_LoadingZone;
+    #endregion
+
+    private void Start()
+    {
+        // I want to know the room the player came from so that I can load them in at the right spot
+        previousRoom = GameStatus.GetInstance().GetPreviousRoom();
+
+        // determine where in the room to spawn players based on the previous room they were in
+        if (previousRoom == "spawn")
+        {
+            player.transform.position = spawnPoint.transform.position;
+        }
+        else if (previousRoom == "MainMenu")  // if you are continuing a previous file, you will spawn in the last room you were in at the main menu location
+        {
+            player.transform.position = spawnPoint.transform.position;
+        }
+        else if (previousRoom == "Overwhelm4")   // repeat this for each transition
+        {
+            player.transform.position = Overwhelm4_LoadingZone.transform.position;
+        }
+        else if (previousRoom == "Words5")   // repeat this for each transition
+        {
+            player.transform.position = Words5_LoadingZone.transform.position;
+        }
+    }
+}
